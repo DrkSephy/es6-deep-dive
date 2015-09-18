da497e720ffc9b9d915505279e7043123e4e95f5
DrkSephy <dleonar00@citymail.cuny.edu>
2015-09-17 21:47:17 -0400

    Updating Point class and main.js

--- es6/Point.js
+++ es6/Point.js
@@ -1,12 +1,13 @@
 class Point {
 	
-	constructor(x, y) {
+	constructor(x, y, z) {
 		this.x = x;
 		this.y = y;
+		this.z = z;
 	}
 
 	toString() {
-		return ' (' + this.x + ' , ' + this.y + ' )';
+		return ' (' + this.x + ' , ' + this.y +  ' , ' + this.z + ')';
 	}
 }
 export default Point;
