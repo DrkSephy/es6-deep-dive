85ed7774df12353fc412ab9e894611497be7f5cb
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-21 07:49:00 -0400

    Adding Sum of Squares equivalent in ES6

---
+++ es6/sumOfSquares.js
@@ -0,0 +1,5 @@
+// Using the arrow function, we have a more concise solution. 
+// Arrow functions are extremely useful to replace callbacks
+// which simply return the result of an expression.
+let arr = [1, 2, 3];
+let squares = arr.map(x => x * x);
