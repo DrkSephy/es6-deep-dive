97124b73bf38a379ab45be3bcf0e458ba3211410
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-26 09:43:00 -0400

    Updating ES6 forEach example to extract both index and value

--- es6/forEach.js
+++ es6/forEach.js
@@ -3,3 +3,8 @@ let arr = ['a', 'b', 'c'];
 for (let item of arr) {
     console.log(item);
 }
+
+// Get both index and value using .entries and destructuring
+for (let [index, item] of arr.entries()) {
+    console.log(index+'. '+item);
+}
