1c72c58df954288d447dc42b164cba8881ee2c29
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-28 15:49:00 -0400

    Adding ES5 example for named parameters

---
+++ es5/namedParameters.js
@@ -0,0 +1,7 @@
+// Example of options object pattern in ES5 for 
+// handling named parameters
+function selectEntries(options) {
+    var start = options.start || 0;
+    var end = options.end || -1;
+    var step = options.step || 1;
+}
