ce5d7b72a0024350f0dd74e8bcf40bf5afc83d41
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-28 15:54:00 -0400

    Adding ES6 equivalent for named parameters

---
+++ es6/namedParameters.js
@@ -0,0 +1,5 @@
+// Once again, destructuring to the rescue. We can pass in an object
+// as a function parameter in ES6 to handle named parameters. 
+function selectEntries({ start=0, end=-1, step=1 }) {
+	// Code here
+}
