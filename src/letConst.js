e3e49aafd8b4fb6088294cfd948be9614c252846
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-15 21:18:28 -0500

    Adding more implementations of let and const

--- src/letConst.js
+++ src/letConst.js
@@ -10,7 +10,24 @@ function getFood(food) {
 	return snack;
 }
 
-getFood(false); 
+getFood(false); // Undefined
+
+// Re-write the above example
+var snack = 'Meow Mix';
+
+function getFood(food) {
+	
+	var snack; // Hoisting
+	
+	if (food) {
+		snack = 'Friskies';
+		return snack;
+	}
+
+	return snack;
+}
+
+getFood(false); // Undefined
 
 // ES6
 
