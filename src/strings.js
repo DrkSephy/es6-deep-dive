0863d5569288d4fc4d096ac168f3e701baa91065
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-15 22:00:11 -0500

    Adding strings example

---
+++ src/strings.js
@@ -0,0 +1,18 @@
+// ES5
+
+var string = 'food';
+
+var substring = 'foo';
+
+// Check if substring is contained in string
+console.log(string.indexOf(substring) > -1);
+
+// ES6
+
+const string = 'food';
+
+const substring = 'foo';
+
+// Check if substring is contained in string
+console.log(string.includes(substring)); // true
+
