895a806ec2ae54d11d01effea997b290aeb114e5
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-16 23:24:38 -0500

    Adding example on various ways of importing and exporting in ES6

---
+++ src/imports.js
@@ -0,0 +1,41 @@
+// main.js
+import { sumTwo, sumThree } from 'math/addition';
+
+// Alternate way to import by providing aliases
+import { 
+	sumTwo as addTwoNumbers,
+	sumThree as addThreeNumbers
+} from 'math/addition';
+
+import * as addition from 'math/addition';
+
+console.log(sumTwo(2, 3));
+
+// Alternate calls (1)
+console.log(addTwoNumbers(2, 3));
+
+// Alternate calls (2)
+console.log(addition.sumTwo(2, 3));
+
+///////////////////////////////////////////////////
+
+// math/addition.js
+function sumTwo(a, b) {
+	return a + b;
+}
+
+function sumThree(a, b) {
+	return a + b + c;
+}
+
+export { sumTwo, sumThree };
+
+// Alternate expoorts
+
+export function sumTwo(a, b) {
+	return a + b;
+}
+
+export function sumThree(a, b) {
+	return a + b + c;
+}
