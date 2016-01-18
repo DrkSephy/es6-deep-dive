19114a03b2e7f01274f9fd879de2463a4d658150
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-17 19:03:36 -0500

    Adding new default parameter example

--- src/defaultParameters.js
+++ src/defaultParameters.js
@@ -1,10 +1,15 @@
 // Handling default parameters in ES5
-function add(x, y) {
+function addTwoNumbers(x, y) {
     x = x || 0;
     y = y || 0;
+    return x + y;
 }
 
 // Handling default parameters in ES6
-function add(x=0, y=0) {
-
+function addTwoNumbers(x=0, y=0) {
+	return x + y;
 }
+
+addTwoNumbers(2, 4); // 6
+addTwoNumbers(2); // 2
+addTwoNumbers(); // 0
