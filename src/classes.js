9350dde139804b63c4ddb2d22c2e3a900898e73e
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-19 22:50:32 -0500

    Added WeakMaps as an example of making data private

--- src/classes.js
+++ src/classes.js
@@ -30,3 +30,41 @@ class Personal extends Person {
 
 const person = new Personal('David', 25, 'male', 'metalhead', 'game development');
 person.incrementAge();
+
+
+ let _counter = new WeakMap();
+    let _action = new WeakMap();
+    class Countdown {
+        constructor(counter, action) {
+            _counter.set(this, counter);
+            _action.set(this, action);
+        }
+        dec() {
+            let counter = _counter.get(this);
+            if (counter < 1) return;
+            counter--;
+            _counter.set(this, counter);
+            if (counter === 0) {
+                _action.get(this)();
+            }
+        }
+    }
+
+// Simulating private data using WeakMaps
+let _age = new WeakMap();
+class Person { 
+	constructor(age) {
+		_age.set(this, age);
+	}
+
+	incrementAge() {
+		let age = _age.get(this);
+		if(age > 50) {
+			console.log('Midlife crisis');
+		}
+	}
+}
+
+> const person = new Person(25);
+> person.incrementAge();
+> Reflect.ownKeys(person); // []
